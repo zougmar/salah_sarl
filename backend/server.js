b5ab84fcb48897f8980949195b6b9a9c146cdc10
omar zouglah <114086583+zougmar@users.noreply.github.com>
2025-12-05 21:49:08 +0100

    Update CORS origin to production URL

--- backend/server.js
+++ backend/server.js
@@ -20,7 +20,7 @@ const PORT = process.env.PORT || 3000;
 // Middleware
 // ----------------------
 app.use(cors({
-  origin: process.env.CLIENT_URL || 'http://localhost:5173',
+  origin: "https://salah-sarl-qrwf-n8a7adyk9-zougmars-projects.vercel.app/" || 'http://localhost:5173',
   credentials: true
 }));
 
